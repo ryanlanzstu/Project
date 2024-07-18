912475823caf9d5315344c02f2aa61874bd569d4
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-07-18 21:42:15 +0100

    add eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    apps: [
-      {
-        name: 'calendar',
-        script: 'bundle',
-        args: 'exec rails server -p 3000 -b 0.0.0.0',
-        env: {
-          RAILS_ENV: 'production'
-        }
+  apps: [
+    {
+      name: "calendar",
+      script: "bundle",
+      args: "exec rails server -b 0.0.0.0 -p 3000",
+      env: {
+        RAILS_ENV: "production"
       }
-    ]
-  };
-  
+    }
+  ]
+}
