152fe2b6c44b7b75e702366b2f2a0c32c27ed0ab
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-07-22 16:14:00 +0100

    added contin delivery 3 #11

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env bash
-sudo apt update && sudo apt install -y nodejs npm
+sudo apt update && sudo apt install nodejs npm -y
 # Install pm2
 sudo npm install -g pm2
 # Stop instances running
-pm2 stop calendar || true
+pm2 stop calendar
 # Change directory
-cd ~/Project/
+cd Project/
 # Install dependencies
 npm install
 # Start app
-pm2 start ./bin/www --name calendar
+pm2 start bin/rails --name calendar -- start
