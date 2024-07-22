2c2f8bec7e47ddf6286c1507ded6b324245ce6e5
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-07-22 15:57:56 +0100

    added contin delivery 3 #8

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt install -y nodejs npm
-
 # Install pm2
 sudo npm install -g pm2
-
-# Stop any running instances of the app
+# Stop instances running
 pm2 stop calendar || true
-
-# Navigate to the project directory
-cd ~/Project
-
+# Change directory
+cd ~/Project/
 # Install dependencies
-bundle install
 npm install
-
-# Run pending migrations
-bundle exec rails db:migrate RAILS_ENV=production
-
-# Start the app
-pm2 start "bundle exec rails server -b 0.0.0.0 -p 3000" --name calendar
+# Start app
+pm2 start ./bin/www --name calendar
