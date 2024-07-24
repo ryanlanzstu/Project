c62901378fc8d92f37b656575a529ad6eed042c2
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-07-24 19:45:23 +0100

    deployment broke, fixing again

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 # Update system and install nodejs and npm
 sudo apt update && sudo apt install nodejs npm -y
 
@@ -11,9 +11,24 @@ pm2 stop calendar
 # Change directory to the project
 cd /home/ubuntu/Project
 
+# Install rbenv and ruby-build if not already installed
+if ! command -v rbenv &> /dev/null; then
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  cd ~/.rbenv && src/configure && make -C src
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  exec $SHELL
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+  exec $SHELL
+fi
+
+# Install the correct Ruby version
+rbenv install -s 3.1.4
+rbenv local 3.1.4
 
 # Install dependencies
-npm install
+bundle install
 
 # Start the app with PM2 in production mode
-pm2 start ./bin/www --name calendar
+pm2 start --name calendar -- bundle exec rails server -b 0.0.0.0 -p 3000 -e production
