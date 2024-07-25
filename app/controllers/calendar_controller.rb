8c048f58c13db98bc25e7d0b51e48a0865d84ac1
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-07-25 18:33:11 +0100

    change calendar functionality #2

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,12 +1,6 @@
 class CalendarController < ApplicationController
   def month
     @date = Date.parse(params.fetch(:date, Date.today.to_s))
-    @tasks = Task.where(start_date: @date.all_month)
-  end
-
-  def update_task_date
-    @task = Task.find(params[:id])
-    @task.update(start_date: params[:start_date])
-    head :ok
+    @tasks = Task.where(created_at: @date.beginning_of_month..@date.end_of_month)
   end
 end
