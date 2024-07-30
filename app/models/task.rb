2ba6de993eb0aa2262d35bacb9dcac77de129cd8
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-07-30 14:48:55 +0100

    adding lists to calendar page #2

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,7 @@ class Task < ApplicationRecord
   belongs_to :list
   belongs_to :user
 
-
-    #Add ranked model but have rank within list
-    include RankedModel
-    ranks :row_order, with_same: :list_id
+  #Add ranked model but have rank within list
+  include RankedModel
+  ranks :row_order, with_same: :list_id
 end
