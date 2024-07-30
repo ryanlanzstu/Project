07f3056aea8f231cda716160a305e5081828d938
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-07-30 21:59:58 +0100

    adding lists to calendar #5 events or lists not working on calendar page heroku test

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -1,21 +1,21 @@
 // app/javascript/controllers/sortable_controller.js
 import { Controller } from "stimulus";
 import Sortable from 'sortablejs';
-import interact from 'interactjs';
 
 export default class extends Controller {
   connect() {
     this.initSortable();
-    this.initResizable();
   }
 
   initSortable() {
-    Sortable.create(this.element, {
+    new Sortable(this.element, {
+      group: this.data.get("group"),
+      animation: 150,
       onEnd: (event) => {
-        let url = this.data.get('url');
+        let url = this.data.get("sortable-url");
         let csrfToken = document.querySelector("[name='csrf-token']").content;
 
-        this.fetchWithTimeout(url, {
+        fetch(url, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -25,7 +25,7 @@ export default class extends Controller {
             id: event.item.dataset.id,
             position: event.newIndex
           })
-        }, 5000) // 5 seconds timeout
+        })
         .then(response => response.json())
         .then(data => {
           console.log('Updated successfully:', data);
@@ -33,37 +33,5 @@ export default class extends Controller {
         .catch(error => console.error('Error updating:', error));
       }
     });
-  }
-
-  initResizable() {
-    interact(this.element.querySelectorAll('.resizable')).resizable({
-      edges: { left: true, right: true, bottom: true, top: true },
-      listeners: {
-        move: function (event) {
-          let { x, y } = event.target.dataset;
-          x = (parseFloat(x) || 0) + event.deltaRect.left;
-          y = (parseFloat(y) || 0) + event.deltaRect.top;
-
-          Object.assign(event.target.style, {
-            width: event.rect.width + 'px',
-            height: event.rect.height + 'px',
-            transform: `translate(${x}px, ${y}px)`
-          });
-
-          event.target.dataset.x = x;
-          event.target.dataset.y = y;
-        }
-      }
-    });
-  }
-
-  // Helper function to handle fetch with timeout
-  fetchWithTimeout(url, options, timeout = 5000) {
-    return Promise.race([
-      fetch(url, options),
-      new Promise((_, reject) =>
-        setTimeout(() => reject(new Error('Timeout')), timeout)
-      )
-    ]);
   }
 }
