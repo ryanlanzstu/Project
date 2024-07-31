cda0cca18be13c6804a59abca61e85d4f1f067ae
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-07-31 14:24:00 +0100

    tasks movable outside of container, unfixed

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -9,30 +9,38 @@ export default class extends Controller {
 
   initSortable() {
     new Sortable(this.element, {
-      group: this.element.dataset.group || 'default',
+      group: {
+        name: this.element.dataset.group || 'default',
+        pull: true,
+        put: function (to) {
+          return to.el.closest('.tasks') !== null;
+        }
+      },
       animation: 150,
       onEnd: (event) => {
         let url = this.element.dataset.sortableUrl;
         let csrfToken = document.querySelector("[name='csrf-token']").content;
 
-        fetch(url, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': csrfToken
-          },
-          body: JSON.stringify({
-            id: event.item.dataset.id,
-            position: event.newIndex,
-            list_id: event.to.dataset.listId,
-            date: event.to.dataset.date
+        if (event.to.dataset.listId || event.to.dataset.date) {
+          fetch(url, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRF-Token': csrfToken
+            },
+            body: JSON.stringify({
+              id: event.item.dataset.id,
+              position: event.newIndex,
+              list_id: event.to.dataset.listId,
+              date: event.to.dataset.date
+            })
           })
-        })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Updated successfully:', data);
-        })
-        .catch(error => console.error('Error updating:', error));
+          .then(response => response.json())
+          .then(data => {
+            console.log('Updated successfully:', data);
+          })
+          .catch(error => console.error('Error updating:', error));
+        }
       }
     });
   }
