c98ff97b4a957e39cfc36837fc7d29aaffb232d7
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-07-31 13:13:27 +0100

    tasks sortable within original list on calendar page

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -1,6 +1,6 @@
 // app/javascript/controllers/sortable_controller.js
 import { Controller } from "stimulus";
-import Sortable from 'sortablejs';
+import Sortable from "sortablejs";
 
 export default class extends Controller {
   connect() {
@@ -9,21 +9,23 @@ export default class extends Controller {
 
   initSortable() {
     new Sortable(this.element, {
-      group: this.data.get("group"),
+      group: this.element.dataset.group || 'default',
       animation: 150,
       onEnd: (event) => {
-        let url = this.data.get("sortable-url");
+        let url = this.element.dataset.sortableUrl;
         let csrfToken = document.querySelector("[name='csrf-token']").content;
 
         fetch(url, {
-          method: 'POST',
+          method: 'PATCH',
           headers: {
             'Content-Type': 'application/json',
             'X-CSRF-Token': csrfToken
           },
           body: JSON.stringify({
             id: event.item.dataset.id,
-            position: event.newIndex
+            position: event.newIndex,
+            list_id: event.to.dataset.listId,
+            date: event.to.dataset.date
           })
         })
         .then(response => response.json())
