50dc9f69a309914bac46da9e226c741ce4518e57
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-08-02 14:39:41 +0100

    added unit tests, phase 1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,18 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should create a user" do
-    user = User.new(email: 'testuser@example.com', password: 'password', password_confirmation: 'password')
-    assert user.save, "User was not saved"
+  test "should work with email & password" do
+    user = User.new(email: 'test@test.com', password: 'password')
+    assert user.valid?
+  end
+
+  test "shouldn't work with no email" do
+    user = User.new(email: nil, password: 'password')
+    assert_not user.valid?
+  end
+
+  test "shouldn't work with no password" do
+    user = User.new(email: 'test@test.com', password: nil)
+    assert_not user.valid?
   end
 end
