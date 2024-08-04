47f9fdb1134558611e84d629f15d23f83389ef10
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-08-04 22:09:46 +0100

    brushing up

--- app/models/collegemodule.rb
+++ app/models/collegemodule.rb
@@ -1,3 +1,4 @@
 class Collegemodule < ApplicationRecord
+    #Adds link to signed in user
     belongs_to :user
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
+    #Adds link to signed in user
     belongs_to :user
 end
